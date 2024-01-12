cadd0c503455e37e38b84fce82805ee7dd1f866f
MateusAugusto <mateusaugusto2017pp@gmail.com>
2024-01-11 21:48:26 -0300

    experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,13 +1,53 @@
-import '../Experience/Experience.css';
+import "../Experience/Experience.css";
+import Subtitle from "../Subtitle/Subtitle";
 
 function Experience() {
-  return(
-    <article>
-      <h3>Experience</h3>
-      <p>I have been working as a freelancer for a year and a half, and I am part of a digital menu startup.</p>
+  return (
+    <article className="experience-container">
+      <Subtitle subtitle={"Experience"} />
+      <p className="experience">
+        I have been working as a freelancer for a year and a half, and I am part
+        of a digital menu startup.
+      </p>
 
-      <div>
-        
+      <div className="experience-time-container">
+
+        <div className="experience-time-layout">
+          <img src="src/assets/icon-html.svg" alt="" />
+          <div className="experience-time-content">
+            <div className="time">
+              1 years
+            </div>
+          </div>
+        </div>
+
+        <div className="experience-time-layout">
+          <img src="src/assets/icon-css.svg" alt="" />
+          <div className="experience-time-content">
+            <div className="time">
+              1 years
+            </div>
+          </div>
+        </div>
+
+        <div className="experience-time-layout">
+          <img src="src/assets/icon-js.svg" alt="" />
+          <div className="experience-time-content">
+            <div className="time">
+              1 years
+            </div>
+          </div>
+        </div>
+
+        <div className="experience-time-layout">
+          <img src="src/assets/icon-tailwind.svg" alt="" />
+          <div className="experience-time-content">
+            <div className="time">
+              1 years
+            </div>
+          </div>
+        </div>
+
       </div>
     </article>
   );
