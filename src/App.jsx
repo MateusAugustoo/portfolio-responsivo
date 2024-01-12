b1ca5254f8748ede3139541cd6b6043306c22175
MateusAugusto <mateusaugusto2017pp@gmail.com>
2024-01-11 21:49:16 -0300

    conclusão layout moblie

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import Contact from "./components/Contact/Contact";
 import Experience from "./components/Experience/Experience";
+import Information from "./components/Information/Information";
 import Header from "./components/Profile/Header";
 
 function App() {
   return (
     <>
-      <main>
+      <main className="layout">
         <Header />
         <Experience />
+        <Information />
+        <Contact />
       </main>
     </>
   );
