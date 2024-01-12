87e8a58b22ef0832f53f684e7e7b7f0b9d047fa6
MateusAugusto <mateusaugusto2017pp@gmail.com>
2024-01-11 21:47:49 -0300

    header component

--- src/components/Profile/Header.jsx
+++ src/components/Profile/Header.jsx
@@ -2,10 +2,10 @@ import '../Profile/Header.css';
 
 function Header () {
   return(
-    <header>
+    <header className='header-container'>
       <div>
         <h1 className='title'>Hi, i'm Mateus! 👋</h1>
-        <p className='subtitle'>Software Developer</p>
+        <p className='work'>Software Developer</p>
       </div>
       <img className='photoProfile' src="https://avatars.githubusercontent.com/u/110947775?v=4" alt="foto do desenvolvedor" />
     </header>
